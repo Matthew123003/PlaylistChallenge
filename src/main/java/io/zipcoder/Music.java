248cbcb118ee2748e9e1cd499d444e01c6d0fe55
Matt Kramer <matthew123003@gmail.com>
2024-03-15 17:41:15 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,56 @@ public class Music {
 
     private String[] playList;
 
+    private Integer startIndex;
+
+    private String selection;
+
+
+
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
+        return Integer.min(nextSong(startIndex, selection), prevSong(startIndex, selection));
+    }
+
+    public Integer nextSong(int start, String sel){
+        int numJumps = 0;
+        for (int i = start; i < playList.length ; i++) {
+            if (playList[i].equals(sel)){
+                return numJumps;
+            }
+            numJumps++;
+        }
+        numJumps++;
+        for (int i = 0; i < start; i++) {
+            if(playList[i].equals(sel)){
+                return numJumps;
+            }
+            numJumps++;
+        }
+        return null;
+    }
+
+    public Integer prevSong(int start, String sel){
+        int numJumps = 0;
+        for (int i = start; i >= 0 ; i--) {
+            if (playList[i].equals(sel)){
+                return numJumps;
+            }
+            if (start == 0) {
+                break;
+            }
+            numJumps++;
+        }
+        numJumps++;
+        for (int i = playList.length - 1; i > start; i--) {
+            if(playList[i].equals(sel)){
+                return numJumps;
+            }
+            numJumps++;
+        }
         return null;
     }
 }
